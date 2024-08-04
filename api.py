6c9f4c0018808178b2be60cadf95ed0439393436
lxgion <60397382+lxgion@users.noreply.github.com>
2024-08-04 23:55:23 +0200

    init commit

---
+++ api.py
@@ -0,0 +1,28 @@
+import requests
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+
+def gameweekFixtures(gameweek: int) -> list:
+    headers = {"X-Auth-Token": API_KEY}
+    fixtures = requests.get(f'http://api.football-data.org/v4/competitions/PL/matches?matchday={gameweek}', headers=headers).json()
+    matches = list()
+    for match in fixtures['matches']:
+        details = {}
+        details['homeTeam'] = match['homeTeam']['shortName']
+        details['awayTeam'] = match['awayTeam']['shortName']
+        details['dateTime'] = match['utcDate']
+        matches.append(details)
+    return matches
+
+def gameweekResults(gameweek: int) -> list:
+    headers = {"X-Auth-Token": API_KEY}
+    fixtures = requests.get(f'http://api.football-data.org/v4/competitions/PL/matches?matchday={gameweek}', headers=headers).json()
+    results = []
+    for result in fixtures['matches']:
+        results.append(result['score']['winner'])
+    return results
