878d21bbc919e1da14db75aa8c49fd0cca1500de
lxgion <60397382+lxgion@users.noreply.github.com>
2024-08-04 23:57:31 +0200

    added on ready

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ TOKEN = os.getenv('TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 tree = discord.app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f'connection with {client.user} established')
